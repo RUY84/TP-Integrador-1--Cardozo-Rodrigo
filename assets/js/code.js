1aa582cbb48ec066b00c868395c2b04aeab88f87
RUY84 <rodrigocardozo84@gmail.com>
2023-11-06 22:44:37 -0300

    Correciones
    Corrección en code.js y tickets.html

--- assets/js/code.js
+++ assets/js/code.js
@@ -4,16 +4,14 @@
 
 function calcular(){
     const monto = 200;
-    const cantidad = document.getElementById('cant').value;
-    const categoria = document.querySelector('input[name="categoria"]:checked').value;
+    const cantidad = document.getElementById('cant');
+    const cate = document.getElementById('categoria');
+    const pagoTotal = document.getElementById('total');
 
     let bonificacion = 0;    
-    if(categoria=="Estudiante"){
+    if(cate=="Estudiante"){
         const valor = cantidad*monto*0.8 - cantidad*monto;
-
-        const totalAPagar = document.querySelector("#total");
-        totalAPagar.value = `Total a pagar: $${valor}`;
-        
+        pagoTotal.textContent = valor;
         
     }
 }
