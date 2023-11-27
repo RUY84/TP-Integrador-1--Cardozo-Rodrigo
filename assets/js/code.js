adab5c7987e845e2588531a2989362a31114bd38
RUY84 <rodrigocardozo84@gmail.com>
2023-11-26 23:50:43 -0300

    Correciones en el JS
    Se corrijen fallas que evitan se ejecute el JS

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,17 +1,40 @@
-/*function limpiar(){
-    document.getElementById().value = "";
-}*/
 
-function calcular(){
+    //REFERENCIAS AL DOM (Primero referenciar y luego asignar a variable)
     const monto = 200;
-    const cantidad = document.getElementById('cant');
-    const cate = document.getElementById('categoria');
-    const pagoTotal = document.getElementById('total');
+    const cant = document.getElementById('cant');
+    const categoria = document.getElementById('categoria'); //apunto al elemento del DOM
+    const total = document.getElementById('total');
+    const calcular = document.getElementById("resumen");
+    
+    
+    
+    //BOTON RESUMEN
+    
+    calcular.addEventListener('click', () => {
+        const cantidad = parseInt(cant.value);
+        const cate = parseInt(categoria.value);
+        let valor=0;  
+    
+        console.log(cate);
+        console.log(cantidad);
 
-    let bonificacion = 0;    
-    if(cate=="Estudiante"){
-        const valor = cantidad*monto*0.8 - cantidad*monto;
-        pagoTotal.textContent = valor;
-        
-    }
-}
+        if(cate === 1){
+            valor = (cantidad*monto*0.2); //Descuento del 80%
+        }else if(cate === 2){
+            valor = (cantidad*monto*0.5); //Descuento del 50%
+        }else if(cate === 3){
+            valor = (cantidad*monto*0.85); //Descuento del 15%
+        }
+
+    const pagoTotal = valor;
+    total.textContent = valor;
+    });
+
+
+    //BORRAR
+
+    const borrarDatos = document.getElementById("borrar");
+    borrarDatos.addEventListener("click", () => {
+        const borrarForm = document.querySelector("form");
+        borrarForm.reset();
+    })
